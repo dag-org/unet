1062eb1c3a23f8e9dfd64d7e012fd3f3bde2f222
David Gold <david.gold.a@gmail.com>
2022-01-16 01:45:59 -0800

    It works

--- run_exp.py
+++ run_exp.py
@@ -10,8 +10,8 @@ from unet.config import BATCH_SIZE
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using {device} device")
 
-# wandb.init()
-# config: Dict = wandb.config
+wandb.init()
+config: Dict = wandb.config
 
 model = unet.UNet().to(device)
 
@@ -19,12 +19,12 @@ bear = dataset.BearDataset(model)
 dataloader = torch.utils.data.DataLoader(bear, batch_size=BATCH_SIZE)
 
 optimizer = torch.optim.SGD(
-    model.parameters(), lr=0.001, momentum=0.9
+    model.parameters(), lr=config["learning_rate"], momentum=config["momentum"]
 )
 loss_fn = torch.nn.CrossEntropyLoss()
 
 
-# for t in range(config["n_epochs"]):
-for t in range(1):
+# for t in range(1):
+for t in range(config["n_epochs"]):
     print(f"Epoch {t+1}\n-------------------------------")
     train.train_loop(dataloader, model, loss_fn, optimizer)
