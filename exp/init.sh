5a730442b971beba4519fba5bf8059c12f7d72c0
David Gold <david.gold.a@gmail.com>
2022-01-16 21:57:47 -0800

    Revert back to normal

--- exp/init.sh
+++ exp/init.sh
@@ -7,6 +7,4 @@ echo "Starting sweep agent task..."
 
 wandb login
 
-# EXP_WANDB_DO_LOG=true wandb agent davidgold/uncategorized/$1 --count 1
-
-ping https://wandb.ai/site
+EXP_WANDB_DO_LOG=true wandb agent davidgold/uncategorized/$1 --count 1
