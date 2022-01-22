f4955b93e0708310ee7e5951d55d4e2baf9ca04e
David Gold <david.gold.a@gmail.com>
2022-01-21 16:24:29 -0800

    Parametrize run count

--- exp/init.sh
+++ exp/init.sh
@@ -7,4 +7,4 @@ echo "Starting sweep agent task..."
 
 wandb login
 
-EXP_WANDB_DO_LOG=true wandb agent davidgold/uncategorized/$1 --count 1
+EXP_WANDB_DO_LOG=true wandb agent davidgold/uncategorized/$1 --count $2
